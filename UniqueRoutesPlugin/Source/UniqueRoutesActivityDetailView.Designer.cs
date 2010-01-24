6e59b6ceb3cbd9d96a0bc1f939ec713f024f5f9b
gerhard.NOSPAM <gerhard.NOSPAM@e64aa0cc-dace-11de-849f-b362a63f070a>
2010-01-24 00:03:19 +0000

    Added Copyright to all .cs files Added COPYING and COPYING.LESSER to all projects Added plugin.xml to all projects
    No updates to project definitions

--- UniqueRoutesPlugin/Source/UniqueRoutesActivityDetailView.Designer.cs
+++ UniqueRoutesPlugin/Source/UniqueRoutesActivityDetailView.Designer.cs
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2007, 2008 Kristian Bisgaard Lassen
+Copyright (C) 2010 Kristian Helkjaer Lassen
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace SportTracksUniqueRoutesPlugin.Source
 {
     partial class UniqueRoutesActivityDetailView
