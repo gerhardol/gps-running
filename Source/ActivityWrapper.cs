ef51006e17ec7b157e1f5e68fd4473ea2104b30c
staffan.e.nilsson <staffan.e.nilsson@e64aa0cc-dace-11de-849f-b362a63f070a>
2010-09-26 16:16:14 +0000

    Two new files needed for the new UI to work

---
+++ Source/ActivityWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using ZoneFiveSoftware.Common.Data.Fitness;
+
+namespace GpsRunningPlugin.Source
+{
+    class ActivityWrapper
+    {
+        private IActivity activity;
+        private Color actColor;
+
+        public IActivity Activity
+        {
+            get
+            {
+                return activity;
+            }
+        }
+
+        public Color ActColor
+        {
+            get
+            {
+                return actColor;
+            }
+        }
+
+        public ActivityWrapper()
+        {
+            activity = null;
+        }
+
+        public ActivityWrapper(IActivity activity, Color color)
+        {
+            this.activity = activity;
+            this.actColor = color;
+        }
+
+    }
+}
