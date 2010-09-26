d4b4df202923c3a7b8007c9b552752f99ae84125
staffan.e.nilsson <staffan.e.nilsson@e64aa0cc-dace-11de-849f-b362a63f070a>
2010-09-26 16:16:14 +0000

    Two new files needed for the new UI to work

---
+++ branches/OverlayUIChange/Source/ActivityLabelProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using ZoneFiveSoftware.Common.Visuals;
+using ZoneFiveSoftware.Common.Data.Fitness;
+
+namespace GpsRunningPlugin.Source
+{
+    class ActivityLabelProvider : TreeList.DefaultLabelProvider
+    {
+        #region ILabelProvider Members
+
+        public override string GetText(object element, ZoneFiveSoftware.Common.Visuals.TreeList.Column column)
+        {
+            ActivityWrapper wrapper = (ActivityWrapper)element;
+
+            if (column.Id == "Date")
+                return wrapper.Activity.StartTime.ToLocalTime().ToString();
+            else if (column.Id == "Colour")
+                return null;
+            else if (column.Id == "Visible")
+                return "";
+            else
+                return base.GetText(wrapper.Activity, column);
+        }
+
+        public override System.Drawing.Image GetImage(object element, TreeList.Column column)
+        {
+            ActivityWrapper wrapper = (ActivityWrapper)element;
+
+            if (column.Id == "Colour")
+            {
+                Bitmap image = new Bitmap(column.Width, 15);
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        image.SetPixel(x, y, wrapper.ActColor);
+                    }
+                }
+                return image;
+            }
+            else
+            {
+                return base.GetImage(wrapper.Activity, column);
+            }
+        }
+
+        #endregion
+    }
+}
