fbbef0978cdcb5dfc4a80a9368a84d01fb137acf
staffan.e.nilsson <staffan.e.nilsson@e64aa0cc-dace-11de-849f-b362a63f070a>
2010-11-18 16:32:26 +0000

    Committed changed way of entering filter value etc

---
+++ Source/InputControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GpsRunningPlugin.Source
+{
+    public partial class InputControl : UserControl
+    {
+        public InputControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
