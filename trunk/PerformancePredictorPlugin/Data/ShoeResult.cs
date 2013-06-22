a201fa48a545703f9dbdb86e223e940b0e4abe23
gerhard.nospam <gerhard.nospam@e64aa0cc-dace-11de-849f-b362a63f070a>
2013-06-22 14:09:28 +0000

    Adding new files for extrapolate of age and shoe (not compiling)

---
+++ trunk/PerformancePredictorPlugin/Data/ShoeResult.cs
@@ -0,0 +1,68 @@
+﻿/*
+Copyright (C) 2010 Staffan Nilsson
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using ZoneFiveSoftware.Common.Data.Fitness;
+using ZoneFiveSoftware.Common.Data.Measurement;
+
+namespace GpsRunningPlugin.Source
+{
+    public class ShoeResult
+    {
+        private IActivity activity;
+
+        public IActivity Activity
+        {
+            get
+            {
+                return activity;
+            }
+        }
+
+        public double Weight;
+        public double AjustedVdot;
+        public TimeSpan EstimatedTime;
+        public double EstimatedSpeed;
+
+        public ShoeResult(IActivity activity, double vdot, float predWeight, float currWeight, 
+            TimeSpan time, double dist)
+        {
+            this.activity = activity;
+            this.Weight = predWeight;
+            this.AjustedVdot = vdot * ShoeFactor(predWeight, currWeight);
+            this.EstimatedTime = Predict.scaleTime(time, Math.Pow(vdot / this.AjustedVdot, 0.83));
+
+            this.EstimatedSpeed = dist / EstimatedTime.TotalSeconds;
+        }
+
+        public static float[] aShoeWeight = new float[] { 0, 0.1f, 0.2f, 0.25f, 0.3f, 0.35f, 0.5f, 1f };
+        public static float DefaultWeight = 0.35f;
+        public static float IdealWeight = 0;
+
+        public static float ShoeFactor(float predWeight, float currWeight)
+        {
+            //Jack Daniels, http://runsmartproject.com/coaching/2012/02/06/how-much-does-shoe-weight-affect-performance/
+            //100g (per shoe?) affect 1%
+            float f = 1 + (currWeight - predWeight) * 1000 / 100 * 0.01f;
+            if (f < 0) { f = 1; }
+            return f;
+        }
+    }
+}
