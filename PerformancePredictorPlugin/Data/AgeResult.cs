f0b5cecb0481998dc696d117f562d827ffe01254
gerhard.nospam <gerhard.nospam@e64aa0cc-dace-11de-849f-b362a63f070a>
2013-06-22 14:09:28 +0000

    Adding new files for extrapolate of age and shoe (not compiling)

---
+++ PerformancePredictorPlugin/Data/AgeResult.cs
@@ -0,0 +1,53 @@
+﻿/*
+Copyright (C) 2010 Staffan Nilsson
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using ZoneFiveSoftware.Common.Data.Fitness;
+using ZoneFiveSoftware.Common.Data.Measurement;
+
+namespace GpsRunningPlugin.Source
+{
+    public class AgeResult
+    {
+        private IActivity activity;
+
+        public IActivity Activity
+        {
+            get
+            {
+                return activity;
+            }
+        }
+
+        public double Age;
+        public TimeSpan EstimatedTime;
+        public double EstimatedSpeed;
+        public static float[] aAge = new float[] { 20, 30, 40, 50, 60, 70, 80 };
+
+        public AgeResult(IActivity activity, float predAge, float currAge, TimeSpan time, double dist)
+        {
+            this.activity = activity;
+            this.Age = predAge;
+            this.EstimatedTime = TimeSpan.FromSeconds(PredictWavaTime.WavaPredict(dist, dist, time, predAge, currAge));
+
+            this.EstimatedSpeed = dist / EstimatedTime.TotalSeconds;
+        }
+    }
+}
---
+++ PerformancePredictorPlugin/Data/ShoeLabelProvider.cs
@@ -0,0 +1,74 @@
+﻿/*
+Copyright (C) 2010 Staffan Nilsson
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using ZoneFiveSoftware.Common.Visuals;
+using ZoneFiveSoftware.Common.Data.Fitness;
+using GpsRunningPlugin.Util;
+using GpsRunningPlugin.Properties;
+
+namespace GpsRunningPlugin.Source
+{
+    class ShoeLabelProvider : TreeList.DefaultLabelProvider
+    {
+        #region ILabelProvider Members
+
+        public static ZoneFiveSoftware.Common.Data.Measurement.Weight.Units shoeUnit; 
+        public override string GetText(object element, ZoneFiveSoftware.Common.Visuals.TreeList.Column column)
+        {
+            ShoeResult wrapper = (ShoeResult)element;
+            switch(column.Id)
+            {
+                case ResultColumnIds.ShoeWeight:
+                    return UnitUtil.Weight.ToString(wrapper.Weight, shoeUnit, "");
+                case ResultColumnIds.AdjustedVDOT:
+                    return present(wrapper.AjustedVdot, 1);
+                case ResultColumnIds.EstimatedTime:
+                    return UnitUtil.Time.ToString(wrapper.EstimatedTime);
+                case ResultColumnIds.EstimatedSpeed:
+                    return UnitUtil.PaceOrSpeed.ToString(Settings.ShowPace, wrapper.EstimatedSpeed);
+                 default:
+                    ActivityInfo actInfo = ActivityInfoCache.Instance.GetInfo(wrapper.Activity);
+                    string text = base.GetText(actInfo, column);
+                    if (text != "")
+                        return text;
+                    else
+                        return base.GetText(wrapper.Activity, column);                    
+            }
+        }
+
+        public override Image GetImage(object element, TreeList.Column column)
+        {
+            //TimePredictionResult wrapper = (TimePredictionResult)element;
+            //return base.GetImage(wrapper.Activity, column);
+            return null;
+        }
+
+        #endregion
+        private static string present(double p, int digits)
+        {
+            string pad = "";
+            for (int i = 0; i < digits; i++)
+            {
+                pad += "0";
+            }
+            return String.Format("{0:0." + pad + "}", p);
+        }
+    }
+}
